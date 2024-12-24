ceb2f18c192d4a8a8e65f06800b20db4ef0d6dd5
SangEun24 <doskxmfldk@gmail.com>
2024-12-24 15:03:02 +0900

    수정 완~

---
+++ PuglyProject/src/main/java/com/kh/pugly/common/model/vo/Address.java
@@ -0,0 +1,16 @@
+package com.kh.pugly.common.model.vo;
+
+public class Address {
+
+	private Long addressNo;
+	private String stateName;
+	private int addressType;
+	private String district;
+	private String stateCode;
+	
+	 
+	
+	
+	
+	
+}
