2f2c361ecf3a1b55705726aa1f294b05b9427591
seohansol <snake1141@naver.com>
2024-12-27 11:32:11 +0900

    개똥망 커밋

--- PuglyProject/src/main/java/com/kh/pugly/member/controller/MemberController.java
+++ PuglyProject/src/main/java/com/kh/pugly/member/controller/MemberController.java
@@ -65,8 +65,11 @@ public class MemberController {
 	
 	@GetMapping("enroll_form.address")
 	public ModelAndView updateFormAddress() {
-		Map<String, Object> category = memberService.selectStateCategory();
-		return mv.setViewNameAndData("member/update_enroll_form", category);
+		List<Address> category = memberService.selectStateCategory();
+		Map<String, Object> map = new HashMap();
+		map.put("asd", map);
+		return mv.setViewNameAndData("member/update_enroll_form", map);
+	}
 	public ModelAndView updateFormAddress(ModelAndView mav) {
 		List<Address> category = memberService.selectStateCategory();
 		mav.setViewName("member/update_enroll_form");
@@ -81,15 +84,16 @@ public class MemberController {
 	
 	@GetMapping("join_enroll_form.member")
 	public ModelAndView joinEnrollForm() {
-		Map<String, Object> category = memberService.selectStateCategory();
-		return mv.setViewNameAndData("member/join_enroll_form", category);
+		List<Address> category = memberService.selectStateCategory();
+		Map<String, Object> map = new HashMap();
+		map.put("asd", map);
+		return mv.setViewNameAndData("member/join_enroll_form", map);
 	}
 	
 	@PostMapping("update.memberInfo")
 	public ModelAndView updateMemberInfo(ModelAndView mv, HttpSession session, Member member) {
 		Member loginMember = (Member)session.getAttribute("loginUser");
 		memberService.updateMember(member, loginMember);
-		memberService.updateMember(member, session);
 		return mv;
 	}
 	
