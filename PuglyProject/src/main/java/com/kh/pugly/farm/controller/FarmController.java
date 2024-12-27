0c194e6294db25330331154c09e4cf6f16afeae9
KimChopl <jsan08020@gmail.com>
2024-12-27 11:03:26 +0900

    asd

--- PuglyProject/src/main/java/com/kh/pugly/farm/controller/FarmController.java
+++ PuglyProject/src/main/java/com/kh/pugly/farm/controller/FarmController.java
@@ -1,11 +1,11 @@
 package com.kh.pugly.farm.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.pugly.common.ModelAndViewUtil;
